bcb3203973b69ad7a7e5617435ca498e19d0dcfa
batsieba <batisebatekle@gmail.com>
2023-12-14 02:50:19 +0400

    update

--- hi/_bzero.c
+++ hi/_bzero.c
@@ -1,10 +1,10 @@
 # include "simpleshellmain.h"
 /**
  * Description - This function sets the first n bytes of the memory
- * area pointed to by s to zero.
+ * area pointed to by memo_pointer to zero.
  * This effectively clears the memory of any previously stored data.
- * @s: A pointer to the memory area to be cleared.
- * @n: The number of bytes to clear
+ * @memo_pointer: A pointer to the memory area to be cleared.
+ * @n_bytes: The number of bytes to clear
  * Return: None (void)
 */
 void _bzero(void *memo_pointer, size_t n_bytes)
--- hi/_calloc.c
+++ hi/_calloc.c
@@ -1,16 +1,26 @@
 #include "simpleshellmain.h"
-
-void	*_calloc(size_t count, size_t size)
+/**
+ * Description - This function allocates contiguous memory for the
+ * specified number of elements of the specified size and initializes
+ * each element to zero. It is intended as a safer alternative to
+ * malloc in cases where you want to ensure the memory is cleared before use.
+ * @numOfElements: The number of elements to allocate (size_t).
+ * @byteSize: The size in bytes of each element (size_t).
+ * Return: A pointer to the allocated memory block, or
+ * NULL if the allocation failed.
+*/
+void *_calloc(size_t numOfElements, size_t byteSize)
 {
-	void	*ptr;
+	void *ptr;
 
-	if ((count >= SIZE_MAX && size > 1) || (count > 1 && size > SIZE_MAX))
+	if ((numOfElements >= SIZE_MAX && byteSize > 1)
+	|| (numOfElements > 1 && byteSize > SIZE_MAX))
 		return (NULL);
-	if (!count || !size)
-		return (malloc(count * size));
-	ptr = malloc(count * size);
+	if (!numOfElements || !byteSize)
+		return (malloc(numOfElements * byteSize));
+	ptr = malloc(numOfElements * byteSize);
 	if (!ptr)
 		return (NULL);
-	_bzero(ptr, size * count);
+	_bzero(ptr, byteSize * numOfElements);
 	return (ptr);
 }
