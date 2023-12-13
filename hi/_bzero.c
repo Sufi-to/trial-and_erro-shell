c5d96791df7aa0ec1981ecdd05941660041b76b2
batsieba <batisebatekle@gmail.com>
2023-12-14 02:38:46 +0400

    update

--- hi/_bzero.c
+++ hi/_bzero.c
@@ -1,6 +1,13 @@
 # include "simpleshellmain.h"
-
-void	_bzero(void *s, size_t n)
+/**
+ * Description - This function sets the first n bytes of the memory
+ * area pointed to by s to zero.
+ * This effectively clears the memory of any previously stored data.
+ * @s: A pointer to the memory area to be cleared.
+ * @n: The number of bytes to clear
+ * Return: None (void)
+*/
+void _bzero(void *memo_pointer, size_t n_bytes)
 {
-	_memset(s, 0, n);
+	_memset(memo_pointer, 0, n_bytes);
 }
